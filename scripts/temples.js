99a0a15916ea0157a399018495f0d9e04913d65c
Heaven <gabrielheavenlydawn@gmail.com>
2025-03-12 13:33:32 +0100

    chore: temple gallery added

--- scripts/temples.js
+++ scripts/temples.js
@@ -5,3 +5,10 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const currentYear = new Date().getFullYear();
+document.querySelector("footer h3").innerText = `© 🤎 Ozioma Heaven Gabriel 🤎 Abuja ${currentYear}`;
+
+
+const lastModified = document.lastModified;
+document.getElementById("date").textContent = lastModified;
